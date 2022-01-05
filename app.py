b28560312a61fa01526f1496c8869d62fb5dfbf0
roymammen <roy.mammen@gmail.com>
2022-01-04 19:03:36 -0800

    updated app . py

--- app.py
+++ app.py
@@ -9,15 +9,29 @@ app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
 conn = "mongodb://localhost:27017"
-client = pymongo.MongoClient(conn)
-db = client.local 
+try:
+    # client = pymongo.MongoClient(
+    #     host = "localhost",
+    #     port = 27017,
+    #     serverSelecionTimeoutMS = 1000
+    # )
+    client = pymongo.MongoClient(conn)
+    db = client.local 
+    client.server_info() # trigger exception if not connected to db!
+
+
+except:
+    print("ERROR - Cannot connect to db")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def index():
 
-    # Find one record of data from the mongo database
-    mars_dict = db.mars_dict.find_one()
+    # Find all record of data from the mongo database
+    mars_dict = db.mars_dict.find({})
+    # mars_data = scraping.scrape_all()
+    # Update the Mongo database using update and upsert=True    
+    # db.mars_dict.insertMany(mars_data)
     # Return template and data
     return render_template("index.html", mars=mars_dict)
 
@@ -25,10 +39,10 @@ def index():
 @app.route("/scrape")
 def scrape():
   
-    mars_dict = db.mars_dict.find({})
+    # mars_dict = db.mars_dict.find({})
     mars_data = scraping.scrape_all()
     # Update the Mongo database using update and upsert=True    
-    mars_dict.update({}, mars_data, upsert=True)
+    db.mars_dict.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
 
 if __name__ == "__main__":
